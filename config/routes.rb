e5b4cda2a9d5879ba744e2f3580bbec5708f9179
yukihyou <guild.aoiraimei.yuki@gmail.com>
2022-02-17 12:18:38 +0900

    Task.7-1 記事に関するAPIのルーティングの実装【Article に関する API 実装】 ## 概要 - routes.rbに追記。タイトル通り。

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      mount_devise_token_auth_for "User", at: "auth", controllers: {
-        registrations: "api/v1/auth/registrations",
-      }
+      mount_devise_token_auth_for "User", at: "auth"
+      resources :articles
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
