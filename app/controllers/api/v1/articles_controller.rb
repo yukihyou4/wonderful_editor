4076472760e2213895a9f0721087800830de96e6
yukihyou <guild.aoiraimei.yuki@gmail.com>
2022-02-21 16:00:38 +0900

    Task.7-3 記事一覧の実装 ## 概要 - 記事のAPI実装に関するFactoryの修正 - 記事一覧のAPIの実装に必要なserializer を追加 - 記事一覧を返すAPI及びテストを実装

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,8 @@
+module Api::V1
+  class ArticlesController < BaseApiController # base_api_contorller を継承
+    def index
+      articles = Article.order(updated_at: :desc)
+      render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
+    end
+  end
+end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -24,6 +24,27 @@ RSpec.describe Article, type: :model do
       expect(article).not_to be_valid
     end
   end
+end
+
+  RSpec.describe "Api::V1::Articles", type: :request do
+    describe "GET /articles" do
+      subject { get(api_v1_articles_path) }
+
+      let!(:article1) { create(:article, updated_at: 1.days.ago) }
+      let!(:article2) { create(:article, updated_at: 2.days.ago) }
+      let!(:article3) { create(:article) }
+
+      it "記事の一覧が取得できる" do
+        subject
+        res = JSON.parse(response.body)
+
+        expect(response).to have_http_status(:ok)
+        expect(res.length).to eq 3
+        expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
+        expect(res[0].keys).to eq ["id", "title", "updated_at", "user" ]
+        expect(res[0]["user"].keys).to eq ["id", "name", "email" ]
+      end
+    end
+  end
 
   # pending "add some examples to (or delete) #{__FILE__}"
-end
