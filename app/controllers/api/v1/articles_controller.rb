771428a0da58f060f6e29b49677cb0f97643300a
yukihyou <guild.aoiraimei.yuki@gmail.com>
2022-02-21 16:55:50 +0900

    rubocopによる修正 ## 概要 - タイトル通り

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,5 +1,6 @@
 module Api::V1
-  class ArticlesController < BaseApiController # base_api_contorller を継承
+  class ArticlesController < BaseApiController
+    # base_api_contorller を継承
     def index
       articles = Article.order(updated_at: :desc)
       render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -26,25 +26,23 @@ RSpec.describe Article, type: :model do
   end
 end
 
-  RSpec.describe "Api::V1::Articles", type: :request do
-    describe "GET /articles" do
-      subject { get(api_v1_articles_path) }
+RSpec.describe "Api::V1::Articles", type: :request do
+  describe "GET /articles" do
+    subject { get(api_v1_articles_path) }
 
-      let!(:article1) { create(:article, updated_at: 1.days.ago) }
-      let!(:article2) { create(:article, updated_at: 2.days.ago) }
-      let!(:article3) { create(:article) }
+    let!(:article1) { create(:article, updated_at: 1.days.ago) }
+    let!(:article2) { create(:article, updated_at: 2.days.ago) }
+    let!(:article3) { create(:article) }
 
-      it "記事の一覧が取得できる" do
-        subject
-        res = JSON.parse(response.body)
+    it "記事の一覧が取得できる" do
+      subject
+      res = JSON.parse(response.body)
 
-        expect(response).to have_http_status(:ok)
-        expect(res.length).to eq 3
-        expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
-        expect(res[0].keys).to eq ["id", "title", "updated_at", "user" ]
-        expect(res[0]["user"].keys).to eq ["id", "name", "email" ]
-      end
+      expect(response).to have_http_status(:ok)
+      expect(res.length).to eq 3
+      expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
+      expect(res[0].keys).to eq ["id", "title", "updated_at", "user"]
+      expect(res[0]["user"].keys).to eq ["id", "name", "email"]
     end
   end
-
-  # pending "add some examples to (or delete) #{__FILE__}"
+end
