7c1edf09fbfdd0707c6d7dbd0f082c41212dfe02
yukihyou <guild.aoiraimei.yuki@gmail.com>
2022-04-04 21:17:03 +0900

    Task. 8 フロントエンドとの繋ぎこみ part.1 ## 概要 - 用意されたzipファイルの中身をローカル環境に置き換え。 - yarn installの実行 - $ bundle exec rails s と $ bin/webpack-dev-server を実行し確認。

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth"
       resources :articles
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
